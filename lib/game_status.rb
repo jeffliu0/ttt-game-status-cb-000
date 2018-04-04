f2febcd66387d013fefd569df31758c233e76c49
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-04 05:30:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -11,11 +11,10 @@ def won?(board)
    match = board.size.times.select{|i| board[i] == "X"}
    match2 = board.size.times.select{|i| board[i] == "O"}
 
-   if match.length >= 3
-      WIN_COMBINATIONS.any?{|combo| (combo && match == match) ||
+   if(match.length >= 3 || match2.length >= 3)
+     return WIN_COMBINATIONS.any?{|combo| (combo && match == match) ||
        combo && match2 == match2}
-   elsif match2.length >= 3
-
+   else
      return nil
    end
 end
