c7544fc6fdc9ef7bfd0dde00c686d211eaeefb6b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-04 04:47:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6],
 [0,3,6], [1,4,7], [2,5,8]]
 
 def won?(board)
-   match = board.size.times.select{|i| board[i] == "X"}
+   match = board.size.times.select{|i| board[i] == "X" || board[i] == "O"}
    puts WIN_COMBINATIONS.include?(match)
    puts match
 end
